b6456d785d04d9a75a9c984b78f7741b776f29c2
joynalabedin88 <naljoy600@gmail.com>
2021-06-05 18:06:34 +0600

    import jquery & bootstrap from vue cli

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+// call jquery and bootstrap 4.0
+import jquery from 'jquery'
+window.jQuery = jquery
+require(["popper.js"], function(p) {
+    window.Popper = p;
+    require('bootstrap');
+});
+
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
